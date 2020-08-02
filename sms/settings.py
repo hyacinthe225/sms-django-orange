dff943815f0cc10b64f5b4b3172de8cf59e9e644
HYACINTHE KOUADIO <53180830+hyacinthe225@users.noreply.github.com>
2020-08-02 13:40:39 +0000

    Update settings.py

--- sms/settings.py
+++ sms/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ef(3fun6$ujl5-cm8y#vh0&z&rw6q#vpz6$%@h@9l9ku-$s$5u'
+SECRET_KEY = '****************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
